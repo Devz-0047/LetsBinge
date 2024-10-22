19f5ac9bf439e3093d86a09284566fc27fc3d7ac
Devz-0047 <devendersingh00747@gmail.com>
2024-10-22 20:59:58 +0530

    Added Links in footer

--- src/ui/Footer.jsx
+++ src/ui/Footer.jsx
@@ -1,6 +1,7 @@
 import logo from "../assets/logo.png";
 import { FaInstagram, FaFacebook } from "react-icons/fa6";
 import { FaGithub, FaTwitter } from "react-icons/fa";
+import { Link } from "react-router-dom";
 function Footer() {
   const d = new Date();
   let year = d.getFullYear();
@@ -9,11 +10,13 @@ function Footer() {
       <div>
         <img src={logo} className="mt-1 h-12 w-[12rem] bg-slate-950 pr-4" />
         <p className="mt-2">Copyright &copy; {year} Let&apos;sBinge </p>
-        <div className="mt-3 flex justify-evenly text-orange-600">
-          <FaInstagram size={28} />
-          <FaFacebook size={28} />
-          <FaTwitter size={28} />
-          <FaGithub size={28} />
+        <div>
+          <Link className="mt-3 flex justify-evenly text-orange-600">
+            <FaInstagram size={28} />
+            <FaFacebook size={28} />
+            <FaTwitter size={28} />
+            <FaGithub size={28} />
+          </Link>
         </div>
       </div>
       <div>
@@ -21,19 +24,23 @@ function Footer() {
           Let&apos;Binge{" "}
         </p>
         <ul>
-          <li className="mt-1">Create Account</li>
-          <li>Log In</li>
-          <li>Pricing</li>
-          <li>API</li>
+          <Link>
+            <li className="mt-1">Create Account</li>
+            <li>Log In</li>
+            <li>Pricing</li>
+            <li>API</li>
+          </Link>
         </ul>
       </div>
       <div>
         <p className="text-[1.12rem] font-semibold text-orange-600">Company</p>
         <ul>
-          <li className="mt-1">About</li>
-          <li>Community</li>
-          <li>Contact</li>
-          <li>Learn</li>
+          <Link>
+            <li className="mt-1">About</li>
+            <li>Community</li>
+            <li>Contact</li>
+            <li>Learn</li>
+          </Link>
         </ul>
       </div>
       <div>
@@ -41,19 +48,23 @@ function Footer() {
           Tools & Resources
         </p>
         <ul>
-          <li className="mt-1">Blog</li>
-          <li>API Docs</li>
-          <li>Library</li>
-          <li>Use Cases</li>
+          <Link>
+            <li className="mt-1">Blog</li>
+            <li>API Docs</li>
+            <li>Library</li>
+            <li>Use Cases</li>
+          </Link>
         </ul>
       </div>
       <div>
         <p className="text-[1.12rem] font-semibold text-orange-600">Policy</p>
         <ul>
-          <li className="mt-1">Terms of Use</li>
-          <li>API Terms of Use</li>
-          <li>Privacy</li>
-          <li>DMCA</li>
+          <Link>
+            <li className="mt-1">Terms of Use</li>
+            <li>API Terms of Use</li>
+            <li>Privacy</li>
+            <li>DMCA</li>
+          </Link>
         </ul>
       </div>
     </footer>
