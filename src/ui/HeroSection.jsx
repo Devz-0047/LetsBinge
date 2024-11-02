0e3fd1bcb0a27522b3977f641e0730c63a5d0f96
Devz-0047 <devendersingh00747@gmail.com>
2024-11-02 20:36:19 +0530

    Completed Library

--- src/ui/HeroSection.jsx
+++ src/ui/HeroSection.jsx
@@ -3,13 +3,14 @@ import { useAuth } from "../hooks/useAuth";
 // import heroSection from "../assets/HeroSection.webp";
 function HeroSection() {
   const { data: session } = useAuth();
+  const unAuthMessage = "Welcome to Let'sBinge, Register Now!";
+  const authMessage = `Welcome back, ${session?.user?.user_metadata?.name?.split(" ")[0]}`;
   return (
     <div className="relative flex h-screen w-full items-center justify-center">
       <div className="absolute inset-0 bg-heroSection bg-cover bg-center bg-no-repeat opacity-75"></div>
-      <p className="relative z-10 text-6xl text-orange-600">
-        Welcome to letsbinge {session?.user?.user_metadata?.name?.split(" ")[0]}
-        , Register Now
-      </p>
+      <h2 className="relative z-10 text-6xl text-orange-600">
+        {session ? authMessage : unAuthMessage}
+      </h2>
     </div>
   );
 }
--- src/ui/LibraryContent.jsx
+++ src/ui/LibraryContent.jsx
@@ -16,7 +16,7 @@ function LibraryContent() {
   const userId = session?.user?.id;
 
   const {
-    data: movies,
+    data: moviesAll,
     error: moviesError,
     isLoading: isMoviesLoading,
   } = useQuery({
@@ -26,7 +26,7 @@ function LibraryContent() {
   });
 
   const {
-    data: series,
+    data: seriesAll,
     error: seriesError,
     isLoading: isSeriesLoading,
   } = useQuery({
@@ -34,6 +34,34 @@ function LibraryContent() {
     queryFn: () => fetchSeriesContentIds(userId),
     enabled: !!userId,
   });
+
+  const [currentPageMovie, setCurrentPageMovie] = useState(1);
+  const moviesPerPage = 12;
+  const movieList = Array.isArray(moviesAll) ? moviesAll : [];
+  const startIndexMovie = (currentPageMovie - 1) * moviesPerPage;
+  const paginatedMovies = movieList.slice(
+    startIndexMovie,
+    startIndexMovie + moviesPerPage,
+  );
+  const handlePageChangeMovie = (pageNumber) => {
+    setCurrentPageMovie(pageNumber);
+  };
+
+  const totalPagesMovie = Math.ceil(movieList.length / moviesPerPage);
+
+  const [currentPageSeries, setCurrentPageSeries] = useState(1);
+  const seriesPerPage = 12;
+  const seriesList = Array.isArray(seriesAll) ? seriesAll : [];
+  const startIndexSeries = (currentPageSeries - 1) * seriesPerPage;
+  const paginatedSeries = seriesList.slice(
+    startIndexSeries,
+    startIndexSeries + seriesPerPage,
+  );
+  const handlePageChangeSeries = (pageNumber) => {
+    setCurrentPageSeries(pageNumber);
+  };
+
+  const totalPagesSeries = Math.ceil(seriesList.length / seriesPerPage);
 
   if (isSessionLoading || isMoviesLoading || isSeriesLoading) {
     return <div>Loading...</div>;
@@ -46,21 +74,26 @@ function LibraryContent() {
   return (
     <div>
       <button
-        className={`rounded-l-md ${contentType === "movie" ? "bg-orange-400" : "bg-orange-300"} px-4 py-1 text-2xl text-orange-900 hover:bg-orange-500 active:bg-orange-500`}
+        className={`rounded-l-md ${
+          contentType === "movie" ? "bg-orange-400" : "bg-orange-300"
+        } px-4 py-1 text-2xl text-orange-900 hover:bg-orange-500 active:bg-orange-500`}
         onClick={() => setContentType("movie")}
       >
         Movies
       </button>
       <button
-        className={`rounded-r-md ${contentType === "tv" ? "bg-orange-400" : "bg-orange-300"} px-4 py-1 text-2xl text-orange-900 hover:bg-orange-500`}
+        className={`rounded-r-md ${
+          contentType === "tv" ? "bg-orange-400" : "bg-orange-300"
+        } px-4 py-1 text-2xl text-orange-900 hover:bg-orange-500`}
         onClick={() => setContentType("tv")}
       >
         Series
       </button>
 
-      <ul>
-        {contentType === "movie" && movies
-          ? movies.map((movie) => (
+      {contentType === "movie" ? (
+        <>
+          <ul className="ml-4 grid grid-cols-6 gap-4 pt-4">
+            {paginatedMovies.map((movie) => (
               <li key={movie.id}>
                 <MovieLib
                   id={movie.id}
@@ -70,20 +103,56 @@ function LibraryContent() {
                   poster_path={movie.poster_path}
                 />
               </li>
-            ))
-          : series &&
-            series.map((serie) => (
-              <li key={serie.id} className="ml-10 grid grid-cols-6 gap-4">
+            ))}
+          </ul>
+          <div className="mt-4 flex justify-center space-x-2">
+            {Array.from({ length: totalPagesMovie }, (_, index) => (
+              <button
+                onClick={() => handlePageChangeMovie(index + 1)}
+                key={index}
+                className={`mb-4 rounded-md border px-3 py-1 ${
+                  currentPageMovie === index + 1
+                    ? "bg-orange-500 text-slate-950 hover:bg-orange-600"
+                    : "bg-orange-300 text-slate-950 hover:bg-orange-400"
+                }`}
+              >
+                {index + 1}
+              </button>
+            ))}
+          </div>
+        </>
+      ) : (
+        <>
+          <ul className="ml-4 grid grid-cols-6 gap-4 pt-4">
+            {paginatedSeries.map((series) => (
+              <li key={series.id}>
                 <SeriesLib
-                  id={serie.id}
-                  title={serie.original_name}
-                  release_date={serie.first_air_date}
-                  vote_average={serie.vote_average}
-                  poster_path={serie.poster_path}
+                  id={series.id}
+                  title={series.original_name}
+                  release_date={series.first_air_date}
+                  vote_average={series.vote_average}
+                  poster_path={series.poster_path}
                 />
               </li>
             ))}
-      </ul>
+          </ul>
+          <div className="mt-4 flex justify-center space-x-2">
+            {Array.from({ length: totalPagesSeries }, (_, index) => (
+              <button
+                onClick={() => handlePageChangeSeries(index + 1)}
+                key={index}
+                className={`mb-4 rounded-md border px-3 py-1 ${
+                  currentPageSeries === index + 1
+                    ? "bg-orange-500 text-slate-950 hover:bg-orange-600"
+                    : "bg-orange-300 text-slate-950 hover:bg-orange-400"
+                }`}
+              >
+                {index + 1}
+              </button>
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 }
